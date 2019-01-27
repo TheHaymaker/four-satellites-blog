35497d529420695e52cd0cc99836636753746372
Brett Haymaker <brett.haymaker@gmail.com>
2019-01-27 14:51:39 -0600

    modifies timing

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Transition as ReactTransition, TransitionGroup } from 'react-transition-group';
 
-const timeout = 700;
+const timeout = 500;
 const getTransitionStyles = {
   entering: {
     position: `absolute`,
