d475556b680d9d45593dea3568be57d95fcd0a0c
Brett Haymaker <brett.haymaker@gmail.com>
2019-02-03 16:09:14 -0600

    updates manifest config

--- config/index.js
+++ config/index.js
@@ -4,14 +4,14 @@ module.exports = {
   siteTitle: 'Four Satellites', // Navigation and Site Title
   siteTitleAlt: 'Four Satellites', // Alternative Site title for SEO
   siteTitleManifest: 'FourSatellites',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://foursatellites.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  siteBanner: '/social/four-satellites.png', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Consulting | Speaking | Education | Design', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
-  siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
+  author: 'Brett Haymaker', // Author for schemaORGJSONLD
+  siteLogo: '/social/four-satellites-logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@minimal', // Twitter Username - Optional
